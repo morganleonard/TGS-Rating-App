bee30756cffa4d77c04af845577b1a79eddf1e50
Morgan Leonard <morganleonardpublic@gmail.com>
2015-09-21 12:49:09 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+//=================== GET home page ====================//
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TGS Calculator' });
+});
+
+//=================== POST Form =====================//
+router.post('/submitForm', function(req, res) {
+
+	console.log("processing GET from '/'");
+	console.log('req.body:');
+	console.log(req.body);
+	
+	//get input values from form and capture in variables
+	var heightFeet = req.body.heightFeet,
+		heightInches = req.body.heightInches,
+		weight = req.body.weight,
+		apeIndex = req.body.apeIndex,
+		bloodAlcohol = req.body.bloodAlcohol,
+		age = req.body.age,
+		temperature = req.body.temperature,
+		humidity = req.body.humidity,
+		tgsGrade = 0;
+
+	//calculate TGS Grade
+
+
+	//send TGS Grade as response
+	res.send(tgsGrade);
+})
+
+module.exports = router;
