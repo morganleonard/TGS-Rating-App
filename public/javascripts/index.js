923ee6836792103b6f3341691a912bfa81e2c883
Morgan Leonard <morganleonardpublic@gmail.com>
2015-09-29 17:21:31 -0700

    added click handler on submit button to trigger lightbox, need to add ligthbox module

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,19 @@
 
 $(document).ready( function () {
 
+	console.log("reading public/javascripts.index/js");
+
 	//enable jQuery UI tabs
 	$("#tabs").tabs();
 
-	console.log("reading public/javascripts.index/js");
+	//submit form and pop up lightbox when form submit button is hit
+	//var submitButton = document.getElementById('submitButton');
+	$('#submitButton').click(function () {
+		//code to launch lightbox
+		//....
+		console.log("In submitButton click handler");
+
+
+	})
 
 })
