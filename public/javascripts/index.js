902c09fccb64068e66b7243a1a0d9ca1b39ba885
Morgan Leonard <morganleonardpublic@gmail.com>
2015-10-27 13:13:38 -0700

    added click handlers for standard and super forms

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,10 +9,10 @@ $(document).ready( function () {
 	//set results window to be closed on page load
 	$("#resultsModal").dialog({ autoOpen: false });
 
-	//submit form and pop up lightbox when form submit button is hit
-	$('#submitButton').click(function (event) {
+	//submit form and pop up lightbox when simple form submit button is hit
+	$('#simpleSubmitButton').click(function (event) {
 
-		console.log("In submitButton click handler");
+		console.log("In simpleSubmitButton click handler");
 
 		//prevent default submit on form
 		console.log("preventing default on button click")
@@ -38,6 +38,69 @@ $(document).ready( function () {
 			});
 
 	})
+
+	//submit form and pop up lightbox when standard form submit button is hit
+	$('#standardSubmitButton').click(function (event) {
+
+		console.log("In standardSubmitButton click handler");
+
+		//prevent default submit on form
+		console.log("preventing default on button click")
+		event.preventDefault();
+
+		//get data from from
+		var formData = $('#standardForm').serialize();
+		console.log(formData);
+
+		//send data via ajax to server
+		$.ajax({
+			type: "POST",
+ 			data: formData,
+		}).done(function(data){
+				console.log('data returned from server');
+				console.log(data);
+
+				//update value of tgsGrade in DOM
+				$("#tgsGrade").html(data.tgsGrade);
+				
+				//open modal
+				$("#resultsModal").dialog("open");
+			});
+
+	})
+
+	//submit form and pop up lightbox when super form submit button is hit
+	$('#superSubmitButton').click(function (event) {
+
+		console.log("In superSubmitButton click handler");
+
+		//prevent default submit on form
+		console.log("preventing default on button click")
+		event.preventDefault();
+
+		//get data from from
+		var formData = $('#superForm').serialize();
+		console.log(formData);
+
+		//send data via ajax to server
+		$.ajax({
+			type: "POST",
+ 			data: formData,
+		}).done(function(data){
+				console.log('data returned from server');
+				console.log(data);
+
+				//update value of tgsGrade in DOM
+				$("#tgsGrade").html(data.tgsGrade);
+				
+				//open modal
+				$("#resultsModal").dialog("open");
+			});
+
+	})
+
+
+
 
 	// //prevent defualt follow of link to tab, hide and show sections as needed instead
 	// $('#simpleSprayLink').click(function (event) {
